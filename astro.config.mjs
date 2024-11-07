1643e3ded13f15b20210e0f23cbcb0fc9c3c3f0d
Stephan Griesel <sgriesel@gmail.com>
2024-11-07 07:08:38 +0100

    update cms fields

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,49 +25,6 @@ export default defineConfig({
         // Configure the content collections
         collections: [
           {
-            name: "galleries",
-            label: "Gallery Posts",
-            label_singular: "Gallery Post",
-            folder: "src/pages/posts",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "starredTag",
-                widget: "boolean",
-                label: "Featured Project",
-                hint: "Enabling this will add photo to top. Minimum 3 projects required.",
-                default: "false",
-              },
-              {
-                name: "thumbnail",
-                widget: "image",
-                choose_url: true,
-                default: "/upload/logogray.svg",
-              },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              { name: "tag", widget: "string", label: "Tag", required: true },
-              { name: "body", widget: "markdown", label: "Post Body" },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../layouts/GalleryPost.astro",
-                options: [
-                  {
-                    label: "Gallery Post",
-                    value: "../../layouts/GalleryPost.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
             name: "hero",
             label: "Hero Intro",
             label_singular: "intro",
@@ -82,6 +39,30 @@ export default defineConfig({
                 label: "Description",
                 required: true,
               },
+              {
+                name: "ctaleft",
+                widget: "string",
+                label: "CTA Left",
+                required: true,
+              },
+              {
+                name: "ctaleftlink",
+                widget: "string",
+                label: "CTA Left URL",
+                required: true,
+              },
+              {
+                name: "ctaright",
+                widget: "string",
+                label: "CTA Right",
+                required: true,
+              },
+              {
+                name: "ctarightlink",
+                widget: "string",
+                label: "CTA Right URL",
+                required: true,
+              },
               { name: "body", widget: "markdown", label: "Post Body" },
               {
                 name: "layout",
@@ -96,179 +77,7 @@ export default defineConfig({
               },
             ],
           },
-          {
-            name: "accommodation",
-            label: "Accommodation",
-            label_singular: "Accommodation",
-            folder: "src/pages/content/accommodation",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              {
-                label: "Hero Image",
-                name: "thumbnail",
-                widget: "image",
-                choose_url: true,
-              },
-              { name: "tag", widget: "string", label: "Tag", required: true },
-              { name: "body", widget: "markdown", label: "Post Body" },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "Gallery Post",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            name: "faq",
-            label: "FAQ",
-            label_singular: "FAQ",
-            folder: "src/pages/content/faq",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              { name: "body", widget: "markdown", label: "Post Body" },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "Gallery Post",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            name: "rsvp",
-            label: "RSVP",
-            label_singular: "rsvp",
-            folder: "src/pages/content/rsvp",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              {
-                label: "Hero Image",
-                name: "RSVPHeroImg",
-                widget: "image",
-                choose_url: true,
-              },
-              { name: "body", widget: "markdown", label: "Post Body" },
-              {
-                label: "Footer Image",
-                name: "RSVPFooterImg",
-                widget: "image",
-                choose_url: true,
-              },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "Gallery Post",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            name: "faqentry",
-            label: "FAQ Entry",
-            label_singular: "FAQ",
-            folder: "src/pages/content/faqentry",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              { name: "body", widget: "markdown", label: "Post Body" },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "Gallery Post",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            name: "navigationbar",
-            label: "Navigation Bar",
-            label_singular: "Navigation Bar",
-            folder: "src/pages/content/navigationbar",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Title" },
-              {
-                name: "logo",
-                widget: "image",
-                label: "Logo",
-                choose_url: true,
-                default: "",
-                required: false,
-              },
-              { name: "menuitem1", widget: "string", label: "Menu Item 1" },
-              { name: "menuitem1url", widget: "string", label: "Url" },
-              { name: "menuitem2", widget: "string", label: "Menu Item 2" },
-              { name: "menuitem2url", widget: "string", label: "Url" },
-              { name: "menuitem3", widget: "string", label: "Menu Item 3" },
-              { name: "menuitem3url", widget: "string", label: "Url" },
-              { name: "menuitem4", widget: "string", label: "Menu Item 4" },
-              { name: "menuitem4url", widget: "string", label: "Url" },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "General",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
+          
         ],
       },
     }),
